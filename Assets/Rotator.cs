b660e2fcf6484932c833f9b4d56115690da2ac35
A.E. Szalo <a.e.szalo@gmail.com>
2017-06-30 08:59:30 +0200

    scene set up

---
+++ Assets/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	
+	void Update () {
+		this.transform.Rotate (0f, 0f, 1f);
+	}
+}
